d7f581662fae1ce6a35c7bb2cf626a89f4041bc0
Aristide Fumagalli <aristide1997@gmail.com>
2024-05-22 11:14:24 +0100

    methods moved to Application class

--- src/versioning.py
+++ src/versioning.py
@@ -1,18 +1,10 @@
 import hashlib
-# from db_operations import DynamoDBOperations
-# from jwt_operations import JWTManager
-# from response_handler import ResponseHandler
-import json
-import jwt
-import datetime
-from botocore.exceptions import ClientError
-import re
 import logging
-# from error_messages import ErrorMessages
+import re
 
-from services import DynamoDBOperations, JWTManager
-from utilities import ResponseHandler, ErrorMessages
 from models import Application
+from services import DynamoDBOperations, JWTManager
+from utilities import ErrorMessages, ResponseHandler
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
@@ -81,29 +73,6 @@ def lambda_handler(event, context, db_operations=None):
         else:
             return response_handler.response(ErrorMessages.INVALID_REQUEST, 400)
 
-    #     app_name = event['pathParameters'].get('app_name')
-    #     if not validate_app_name(app_name):
-    #         return response_handler.response(ErrorMessages.INVALID_APP_NAME, 400)
-        
-    #     if check_if_secure_app(db_operations, app_name):
-    #         token = headers.get('Authorization')
-    #         if not token or not jwt_manager.verify_jwt(token, db_operations, app_name):
-    #             return response_handler.response(ErrorMessages.UNAUTHORIZED, 401)
-
-    #     # rest of routing logic
-    #     if path == "/api/{app_name}/version" and operation == "GET":
-    #         return get_version(db_operations, app_name)
-    #     elif path == "/api/{app_name}/bump" and operation == "POST":
-    #         version_type = params.get('type')
-    #         return bump_version(db_operations, app_name, version_type)
-    #     elif path == "/api/{app_name}/set" and operation == "POST":
-    #         new_version = params.get('new_version')
-    #         if not validate_version(new_version):
-    #             return response_handler.response(ErrorMessages.INVALID_VERSION_TYPE, 400)
-    #         return set_version(db_operations, app_name, new_version)
-    #     else:
-    #         return response_handler.response(ErrorMessages.INVALID_REQUEST, 400)
-
     except Exception as e:
         return response_handler.response(ErrorMessages.format_error(ErrorMessages.INTERNAL_SERVER_ERROR, str(e)), 500)
 
@@ -153,67 +122,6 @@ def create_app(db_operations, app_name, secure=False, expiry_days=365):
             response_data['token'] = token
 
         return response_handler.response(response_data, 201)
-    except Exception as e:
-        return response_handler.response(ErrorMessages.format_error(ErrorMessages.INTERNAL_SERVER_ERROR, str(e)), 500)
-
-
-def get_version(db_operations, app_name):
-    """
-    Retrieves the current version of the specified app from the database.
-    """
-    try:
-        result = db_operations.get_item({'appName': app_name})
-        if 'Item' not in result:
-            return response_handler.response(ErrorMessages.APP_NOT_FOUND, 404)
-        return response_handler.response({'version': result['Item']['version']}, 200)
-    except Exception as e:
-        return response_handler.response(ErrorMessages.format_error(ErrorMessages.INTERNAL_SERVER_ERROR, str(e)), 500)
-
-def bump_version(db_operations, app_name, version_type):
-    """
-    Bumps the app version based on the specified type (major, minor, or patch).
-    """
-    if not version_type or version_type not in ['major', 'minor', 'patch']:
-        return response_handler.response(ErrorMessages.INVALID_VERSION_TYPE, 400)
-
-    try:
-        result = db_operations.get_item({'appName': app_name})
-        if 'Item' not in result:
-            return response_handler.response(ErrorMessages.APP_NOT_FOUND, 404)
-        
-        current_version = result['Item']['version']
-        major, minor, patch = [int(part) for part in current_version.split('.')]
-        new_version = ''
-        if version_type == 'major':
-            new_version = f"{major + 1}.0.0"
-        elif version_type == 'minor':
-            new_version = f"{major}.{minor + 1}.0"
-        else:  # patch
-            new_version = f"{major}.{minor}.{patch + 1}"
-
-        db_operations.update_item(
-            {'appName': app_name},
-            'SET version = :v',
-            {':v': new_version}
-        )
-        return response_handler.response({'new_version': new_version}, 200)
-    except Exception as e:
-        return response_handler.response(ErrorMessages.format_error(ErrorMessages.INTERNAL_SERVER_ERROR, str(e)), 500)
-
-def set_version(db_operations, app_name, new_version):
-    """
-    Sets the version of an app to a specified new value.
-    """
-    if not new_version:
-        return response_handler.response(ErrorMessages.MISSING_NEW_VERSION, 400)
-
-    try:
-        db_operations.update_item(
-            {'appName': app_name},
-            'SET version = :v',
-            {':v': new_version}
-        )
-        return response_handler.response({'new_version': new_version}, 200)
     except Exception as e:
         return response_handler.response(ErrorMessages.format_error(ErrorMessages.INTERNAL_SERVER_ERROR, str(e)), 500)
 
